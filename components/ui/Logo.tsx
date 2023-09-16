6d0067dc15904a53b9abf37696fd0097df07e903
jurecapuder <capuder.j@gmail.com>
2023-09-16 04:28:21 +0200

    Updating logo and navbar

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const Logo = () => {
-  return <h1 className='text-2xl font-bold bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent'>
-    RemindMe
-  </h1>
+  return (
+    <h1 className='text-2xl font-bold bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent'>
+      RemindMe
+    </h1>
+  )
 }
 
 export default Logo
--- components/ui/NavBar.tsx
+++ components/ui/NavBar.tsx
@@ -6,13 +6,15 @@ import ThemeSwitcher from './ThemeSwitcher';
 import Logo from './Logo';
 
 const NavBar = () => {
-  return <nav className='flex w-full items-center justify-between p-4 px-8 h-[60px]'>
-    <Logo />
-    <div className='flex gap-4 items-center'>
-      <UserButton afterSignOutUrl="/" />
-      <ThemeSwitcher />
-    </div>
-  </nav>
+  return (
+    <nav className='flex w-full items-center justify-between p-4 px-8 h-[60px]'>
+      <Logo />
+      <div className='flex gap-4 items-center'>
+        <UserButton afterSignOutUrl="/" />
+        <ThemeSwitcher />
+      </div>
+    </nav>
+  )
 }
 
 export default NavBar
