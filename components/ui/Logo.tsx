010c6f7f0c54bf5aaacb52c5d6efaa8c93947a33
jurecapuder <capuder.j@gmail.com>
2023-09-16 04:11:53 +0200

    Updating logo

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const Logo = () => {
-  return (
-    <div>Logo</div>
-  )
+  return <h1 className='text-2xl font-bold bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent'>
+    RemindMe
+  </h1>
 }
 
 export default Logo
