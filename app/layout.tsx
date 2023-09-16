c48f3926ea8fe1235c3b52061d5d2c0673b4478a
jurecapuder <capuder.j@gmail.com>
2023-09-16 04:28:03 +0200

    Updating layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
-import { ClerkProvider } from '@clerk/nextjs'
-import { Separator } from '@/components/ui/separator'
-import NavBar from '@/components/ui/NavBar'
+import { ClerkProvider } from '@clerk/nextjs';
+import { Separator } from '@/components/ui/separator';
+import NavBar from '@/components/ui/NavBar';
+import { ThemeProvider } from '@/providers/ThemeProvider';
+import { cn } from '@/lib/utils';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,13 +22,15 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>
-          <div className='flex min-h-screen w-full flex-col items-center dark:bg-black'>
-            <NavBar />
-            <Separator />
-            {children}
-          </div>
+      <html lang="en" className={cn(inter.className)}>
+        <body>
+          <ThemeProvider>
+            <div className='flex min-h-screen w-full flex-col items-center dark:bg-black'>
+              <NavBar />
+              <Separator />
+              {children}
+            </div>
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
