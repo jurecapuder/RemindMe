1e69aeee6e2cda088f16a355c19aea81baa09ddc
jurecapuder <capuder.j@gmail.com>
2023-09-17 23:14:10 +0200

    Updating page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,42 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import { wait } from "@/lib/wait";
 import { currentUser } from "@clerk/nextjs"
+import { Suspense } from "react";
 
 export default async function Home() {
+  return (
+    <>
+      <Suspense fallback={<WelcomeMsgFallback />}>
+        <WelcomeMsg />
+      </Suspense>
+    </>
+  );
+}
+
+async function WelcomeMsg() {
   const user = await currentUser();
+  await wait(3000)
 
   if (!user) {
     return <div>Error</div>
   }
 
   return (
-    <div>
-      Welcome, <br/> {user.firstName} {user.lastName}
+    <div className="flex w-full">
+      <h1 className="text-4xl font-bold">
+        Welcome, <br/> {user.firstName} {user.lastName}
+      </h1>
+    </div>
+  );
+}
 
+function WelcomeMsgFallback() {
+  return (
+    <div className="flex w-full">
+      <h1 className="text-4xl font-bold">
+        <Skeleton className="w-[150px] h-[36px]" />
+        <Skeleton className="w-[150px] h-[36px]" />
+      </h1>
     </div>
   )
 }
