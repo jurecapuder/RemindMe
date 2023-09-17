74203a8e6449af52a7eefd256913ed96f507c2eb
jurecapuder <capuder.j@gmail.com>
2023-09-17 23:05:26 +0200

    Wrapping children in main

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,9 @@ export default function RootLayout({
             <div className='flex min-h-screen w-full flex-col items-center dark:bg-black'>
               <NavBar />
               <Separator />
+              <main className='flex flex-grow w-full justify-center dark:bg-neutral-950'>
               {children}
+              </main>
             </div>
           </ThemeProvider>
         </body>
