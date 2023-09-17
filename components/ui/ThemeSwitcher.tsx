c95e6c40a635dfd4979e29a0d7dc91eff93b93e9
jurecapuder <capuder.j@gmail.com>
2023-09-17 22:33:21 +0200

    Updating theme switcher

--- components/ui/ThemeSwitcher.tsx
+++ components/ui/ThemeSwitcher.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+"use client";
+
+import { useTheme } from 'next-themes';
+import React, { useEffect, useState } from 'react';
+import { Tabs, TabsList, TabsTrigger } from './tabs';
 
 const ThemeSwitcher = () => {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  // useEffect only runs on the client, so now we can safely show the UI
+  useEffect(() => {
+    setMounted(true)
+  }, []);
+
   return (
-    <div>ThemeSwitcher</div>
+    <Tabs defaultValue={theme}>
+      <TabsList>
+        <TabsTrigger value="light"></TabsTrigger>
+        <TabsTrigger value="dark"></TabsTrigger>
+        <TabsTrigger value="system"></TabsTrigger>
+      </TabsList>
+    </Tabs>
   )
 }
 
