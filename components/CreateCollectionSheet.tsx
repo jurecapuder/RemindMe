69f8dcebc4b6b589b2101ab496474eacb486a99a
jurecapuder <capuder.j@gmail.com>
2023-09-19 15:17:47 +0200

    Adding sheet components

--- components/CreateCollectionSheet.tsx
+++ components/CreateCollectionSheet.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
-import { Sheet, SheetContent } from './ui/sheet';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle
+} from './ui/sheet';
 
 interface Props {
   open: boolean;
@@ -10,7 +16,15 @@ function CreateCollectionSheet({ open, onOpenChange }: Props) {
   return (
     <Sheet open={open} onOpenChange={onOpenChange}>
       <SheetContent>
-        Test
+        <SheetHeader>
+          <SheetTitle>
+            Add new collection
+          </SheetTitle>
+
+          <SheetDescription>
+            Collections are a away to group your tasks.
+          </SheetDescription>
+        </SheetHeader>
       </SheetContent>
     </Sheet>
   )
