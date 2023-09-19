7ff57f3c34b1c329ae138fd484bbc659b179394e
jurecapuder <capuder.j@gmail.com>
2023-09-19 15:08:38 +0200

    Adding border gradient

--- components/CreateCollectionBtn.tsx
+++ components/CreateCollectionBtn.tsx
@@ -4,13 +4,15 @@ import { Button } from './ui/button';
 
 function CreateCollectionBtn() {
   return (
-    <Button
-      variant={"outline"}
-      className='dark:text-white w-full dark:bg-neutral-950 bg-white'>
-      <span className='bg-gradient-to-r from-red-500 to-orange-500 hover:to-orange-800 bg-clip-text text-transparent'>
-        Create collection
-      </span>
-    </Button>
+    <div className='w-full rounded bg-gradient-to-r from-pink-500 vai-red-500 to-yellow-500 p-[1px]'>
+      <Button
+        variant={"outline"}
+        className='dark:text-white w-full dark:bg-neutral-950 bg-white'>
+        <span className='bg-gradient-to-r from-red-500 to-orange-500 hover:to-orange-800 bg-clip-text text-transparent'>
+          Create collection
+        </span>
+      </Button>
+    </div>
   )
 }
 
