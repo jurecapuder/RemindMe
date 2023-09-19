ec5ec4a00c013efb68d353b00206f9048d33d4ad
jurecapuder <capuder.j@gmail.com>
2023-09-19 15:12:03 +0200

    Updating button

--- components/CreateCollectionBtn.tsx
+++ components/CreateCollectionBtn.tsx
@@ -1,17 +1,22 @@
 "use client";
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from './ui/button';
+import CreateCollectionSheet from './CreateCollectionSheet';
 
 function CreateCollectionBtn() {
+  const [open, setOpen] = useState(false);
+  const handleOpenChange = (open:boolean) => setOpen(open);
   return (
     <div className='w-full rounded bg-gradient-to-r from-pink-500 vai-red-500 to-yellow-500 p-[1px]'>
       <Button
         variant={"outline"}
         className='dark:text-white w-full dark:bg-neutral-950 bg-white'>
-        <span className='bg-gradient-to-r from-red-500 to-orange-500 hover:to-orange-800 bg-clip-text text-transparent'>
+        <span className='bg-gradient-to-r from-red-500 to-orange-500 hover:to-orange-800 bg-clip-text text-transparent'
+        onClick={() => setOpen(true)}>
           Create collection
         </span>
       </Button>
+      <CreateCollectionSheet open={open} onOpenChange={handleOpenChange} />
     </div>
   )
 }
