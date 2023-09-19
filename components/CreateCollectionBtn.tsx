42cafc98eee7cd996baf3b87b0cef42386cb79b8
jurecapuder <capuder.j@gmail.com>
2023-09-19 14:55:42 +0200

    Adding collection button properties

--- components/CreateCollectionBtn.tsx
+++ components/CreateCollectionBtn.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+"use client";
+import React from 'react';
+import { Button } from './ui/button';
 
 function CreateCollectionBtn() {
   return (
-    <div>CreateCollectionBtn</div>
+    <Button
+      variant={"outline"}
+      className='dark:text-white w-full dark:bg-neutral-950 bg-white'>
+      <span className='bg-gradient-to-r from-red-500 to-orange-500 hover:to-orange-800 bg-clip-text text-transparent'>
+        Create collection
+      </span>
+    </Button>
   )
 }
 
