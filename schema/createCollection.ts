859cfe876559db846f9935f7d9ec2a470ad1416d
jurecapuder <capuder.j@gmail.com>
2023-09-21 19:16:52 +0200

    Updating create collection

--- schema/createCollection.ts
+++ schema/createCollection.ts
@@ -1,8 +1,11 @@
+import { CollectionColors } from '@/lib/constants';
 import { z } from 'zod';
 
 export const createCollectionSchema = z.object({
   name: z.string().min(4, {
       message: 'Collection name must be at least 4 characters long'
     }),
-  color: z.string(),
+  color: z.string().refine(color => Object.keys(CollectionColors).includes(color))
 });
+
+export type createCollectionSchemaType = z.infer<typeof createCollectionSchema>;
