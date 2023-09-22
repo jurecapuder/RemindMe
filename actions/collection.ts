c572339c48d5b97722a2bf901a8a66061e5a11ef
jurecapuder <capuder.j@gmail.com>
2023-09-22 22:12:25 +0200

    Adding create collection as server action

---
+++ actions/collection.ts
@@ -0,0 +1,19 @@
+import prisma from "@/lib/prisma";
+import { createCollectionSchemaType } from "@/schema/createCollection";
+import { currentUser } from "@clerk/nextjs";
+
+export async function createCollection(form: createCollectionSchemaType) {
+  const user = await currentUser();
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  return await prisma.collection.create({
+    data: {
+      userId: user.id,
+      color: form.color,
+      name: form.name,
+    }
+  });
+}
