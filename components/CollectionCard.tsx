c7581d2d6cd3b8c7e84cb47177224a3f08975b33
jurecapuder <capuder.j@gmail.com>
2023-09-24 21:05:07 +0200

    Adding collasible content

--- components/CollectionCard.tsx
+++ components/CollectionCard.tsx
@@ -2,15 +2,18 @@
 
 import { Collection } from '@prisma/client';
 import React, { useState } from 'react';
-import { Collapsible, CollapsibleTrigger } from './ui/collapsible';
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from './ui/collapsible';
 import { Button } from './ui/button';
 import { cn } from '@/lib/utils';
 import { CollectionColor, CollectionColors } from '@/lib/constants';
 import { CaretDownIcon, CaretUpIcon } from '@radix-ui/react-icons';
+import { Progress } from './ui/progress';
 
 interface Props {
   collection: Collection;
 }
+
+const tasks: string[] = ["Task 1", "Task 2"];
 
 function CollectionCard({ collection }: Props) {
   const [isOpen, setIsOpen] = useState(true);
@@ -31,8 +34,27 @@ function CollectionCard({ collection }: Props) {
           {!isOpen && <CaretDownIcon className='h-6 w-6' />}
 
           {isOpen && <CaretUpIcon className='h-6 w-6' />}
+
         </Button>
       </CollapsibleTrigger>
+
+      <CollapsibleContent className='flex rounded-b-md flex-col dark:bg-neutral-900 shadow-lg'>
+        {tasks.length === 0 && <div>No tasks</div>}
+
+        {
+          tasks.length > 0 && (
+            <>
+              <Progress />
+
+              <div>
+                {tasks.map((task) => (
+                  <div>Mocked task</div>
+                ))}
+              </div>
+            </>
+          )
+        }
+      </CollapsibleContent>
     </Collapsible>
   )
 }
