cdeb86daba2767830d4c1d9931901dbd5dc776a4
jurecapuder <capuder.j@gmail.com>
2023-09-24 20:55:21 +0200

    Updating page layout

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -75,12 +75,13 @@ async function CollectionList() {
   }
 
   return (
-    <div>
+    <>
       <CreateCollectionBtn />
-      
-      {collections.map((collection) => (
-        <CollectionCard key={collection.id} collection={collection} />
-      ))}
-    </div>
+      <div className="flex flex-col gap-4 mt-6">
+        {collections.map((collection) => (
+          <CollectionCard key={collection.id} collection={collection} />
+        ))}
+      </div>
+    </>
   )
 }
