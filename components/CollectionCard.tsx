dfb1d458bde81856ab83fe8f923ca43cfe9fd17d
jurecapuder <capuder.j@gmail.com>
2023-09-24 20:45:06 +0200

    Adding collapsible item property

--- components/CollectionCard.tsx
+++ components/CollectionCard.tsx
@@ -1,13 +1,32 @@
-import { Collection } from '@prisma/client'
-import React from 'react'
+"use client";
+
+import { Collection } from '@prisma/client';
+import React, { useState } from 'react';
+import { Collapsible, CollapsibleTrigger } from './ui/collapsible';
+import { Button } from './ui/button';
+import { cn } from '@/lib/utils';
+import { CollectionColor, CollectionColors } from '@/lib/constants';
 
 interface Props {
   collection: Collection;
 }
 
 function CollectionCard({ collection }: Props) {
+  const [isOpen, setIsOpen] = useState(true);
+
   return (
-    <div>{collection.name}</div>
+    <Collapsible open={isOpen} onOpenChange={setIsOpen}>
+      <CollapsibleTrigger>
+        <Button
+          variant={"ghost"}
+          className={cn("flex w-full justify-between p-6",
+          CollectionColors[collection.color as CollectionColor]
+          )}
+        >
+          {collection.name}
+        </Button>
+      </CollapsibleTrigger>
+    </Collapsible>
   )
 }
 
