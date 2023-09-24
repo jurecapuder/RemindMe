88dfa2a23c3f24bc15ee0f9df2a141a0b43dcc64
jurecapuder <capuder.j@gmail.com>
2023-09-24 22:04:21 +0200

    Adding delete collection server action

--- actions/collection.ts
+++ actions/collection.ts
@@ -18,3 +18,18 @@ export async function createCollection(form: createCollectionSchemaType) {
     }
   });
 }
+
+export async function deleteCollection(id: number) {
+  const user = await currentUser();
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  return await prisma.collection.delete({
+    where: {
+      id: id,
+      userId: user.id
+    }
+  });
+}
