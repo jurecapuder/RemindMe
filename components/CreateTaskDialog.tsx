5ced87ad12e5c0d5bded7ecaadcf7a5e68eee5f2
jurecapuder <capuder.j@gmail.com>
2023-09-25 23:54:02 +0200

    Updating create task dialog

--- components/CreateTaskDialog.tsx
+++ components/CreateTaskDialog.tsx
@@ -3,6 +3,8 @@
 import { Collection } from '@prisma/client';
 import React from 'react';
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from './ui/dialog';
+import { cn } from '@/lib/utils';
+import { CollectionColor, CollectionColors } from '@/lib/constants';
 
 interface Props {
   open: boolean;
@@ -20,9 +22,13 @@ function CreateTaskDialog({ open, setOpen, collection }: Props) {
       <DialogContent>
         <DialogHeader>
           <DialogTitle>
-            Add task to collection
+            Add task to collection: {" "}
 
-            <span>
+            <span className={cn(
+              "p-[1px] bg-clip-text text-transparent",
+              CollectionColors[collection.color as CollectionColor]
+              )}
+            >
               {collection.name}
             </span>
           </DialogTitle>
