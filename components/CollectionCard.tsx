bbc8b51e05d543c56ff8b741580acd67ccf11524
jurecapuder <capuder.j@gmail.com>
2023-09-25 23:45:23 +0200

    Updating collection card

--- components/CollectionCard.tsx
+++ components/CollectionCard.tsx
@@ -26,6 +26,8 @@ function CollectionCard({ collection }: Props) {
 
   const router = useRouter();
 
+  const [showCreateModal, setShowCreateModal] = useState(false);
+
   const tasks = collection.tasks;
 
   const [isLoading, startTransition] = useTransition();
@@ -48,94 +50,100 @@ function CollectionCard({ collection }: Props) {
   }
 
   return (
-    <Collapsible open={isOpen} onOpenChange={setIsOpen}>
-      <CollapsibleTrigger asChild>
-        <Button
-          variant={"ghost"}
-          className={cn("flex w-full justify-between p-6",
-          isOpen && "rounded-b-none",
-          CollectionColors[collection.color as CollectionColor]
-          )}
-        >
-          <span className='text-white font-bold'>
-            {collection.name}
-          </span>
+    <>
+      <Collapsible open={isOpen} onOpenChange={setIsOpen}>
+        <CollapsibleTrigger asChild>
+          <Button
+            variant={"ghost"}
+            className={cn("flex w-full justify-between p-6",
+            isOpen && "rounded-b-none",
+            CollectionColors[collection.color as CollectionColor]
+            )}
+          >
+            <span className='text-white font-bold'>
+              {collection.name}
+            </span>
 
-          {!isOpen && <CaretDownIcon className='h-6 w-6' />}
+            {!isOpen && <CaretDownIcon className='h-6 w-6' />}
 
-          {isOpen && <CaretUpIcon className='h-6 w-6' />}
+            {isOpen && <CaretUpIcon className='h-6 w-6' />}
 
-        </Button>
-      </CollapsibleTrigger>
+          </Button>
+        </CollapsibleTrigger>
 
-      <CollapsibleContent className='flex rounded-b-md flex-col dark:bg-neutral-900 shadow-lg'>
-        {tasks.length === 0 && <div>No tasks</div>}
+        <CollapsibleContent className='flex rounded-b-md flex-col dark:bg-neutral-900 shadow-lg'>
+          {tasks.length === 0 && <div>No tasks</div>}
 
-        {
-          tasks.length > 0 && (
-            <>
-              <Progress className='rounded-none' value={45} />
+          {
+            tasks.length > 0 && (
+              <>
+                <Progress className='rounded-none' value={45} />
 
-              <div className="p-4 gap-3 flex flex-col">
-                {tasks.map((task) => (
-                  <div key={task.id}>
-                    {task.content}
-                  </div>
-                ))}
+                <div className="p-4 gap-3 flex flex-col">
+                  {tasks.map((task) => (
+                    <div key={task.id}>
+                      {task.content}
+                    </div>
+                  ))}
+                </div>
+              </>
+            )
+          }
+
+          <Separator />
+
+          <footer className='h-[40px] px-4 p-[2px] text-xs text-neutral-500 flex justify-between items-center'>
+            <p>
+              Created at {collection.createdAt.toDateString()}
+            </p>
+
+            {isLoading && <div>Deleting...</div>}
+
+            {!isLoading && (
+              <div>
+                <Button
+                  size={"icon"}
+                  variant={"ghost"}
+                  onClick={() => {setShowCreateModal(true)}}
+                >
+                  <PlusIcon />
+                </Button>
+
+                <AlertDialog>
+                  <AlertDialogTrigger asChild>
+                    <Button size={"icon"} variant={"ghost"}>
+                      <TrashIcon />
+                    </Button>
+                  </AlertDialogTrigger>
+
+                  <AlertDialogContent>
+                    <AlertDialogTitle>
+                      Are you absolutely sure?
+                    </AlertDialogTitle>
+
+                    <AlertDialogDescription>
+                      This action cannot be undone. This will permanently delete the <span className='font-bold'>{collection.name}</span> collection and all tasks inside it.
+                    </AlertDialogDescription>
+
+                    <AlertDialogFooter>
+                      <AlertDialogCancel>
+                        Cancel
+                      </AlertDialogCancel>
+
+                      <AlertDialogAction onClick={() => {
+                        startTransition(removeCollection);
+                      }}>
+                        Proceed
+                      </AlertDialogAction>
+                    </AlertDialogFooter>
+                  </AlertDialogContent>
+                </AlertDialog>
               </div>
-            </>
-          )
-        }
-
-        <Separator />
-
-        <footer className='h-[40px] px-4 p-[2px] text-xs text-neutral-500 flex justify-between items-center'>
-          <p>
-            Created at {collection.createdAt.toDateString()}
-          </p>
-
-          {isLoading && <div>Deleting...</div>}
-
-          {!isLoading && (
-            <div>
-              <Button size={"icon"} variant={"ghost"}>
-                <PlusIcon />
-              </Button>
-
-              <AlertDialog>
-                <AlertDialogTrigger asChild>
-                  <Button size={"icon"} variant={"ghost"}>
-                    <TrashIcon />
-                  </Button>
-                </AlertDialogTrigger>
-
-                <AlertDialogContent>
-                  <AlertDialogTitle>
-                    Are you absolutely sure?
-                  </AlertDialogTitle>
-
-                  <AlertDialogDescription>
-                    This action cannot be undone. This will permanently delete the <span className='font-bold'>{collection.name}</span> collection and all tasks inside it.
-                  </AlertDialogDescription>
-
-                  <AlertDialogFooter>
-                    <AlertDialogCancel>
-                      Cancel
-                    </AlertDialogCancel>
-
-                    <AlertDialogAction onClick={() => {
-                      startTransition(removeCollection);
-                    }}>
-                      Proceed
-                    </AlertDialogAction>
-                  </AlertDialogFooter>
-                </AlertDialogContent>
-              </AlertDialog>
-            </div>
-          )}
-        </footer>
-      </CollapsibleContent>
-    </Collapsible>
+            )}
+          </footer>
+        </CollapsibleContent>
+      </Collapsible>
+    </>
   )
 }
 
