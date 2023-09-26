560a65871585ab2b1c551d9ff039715df88b6fb0
jurecapuder <capuder.j@gmail.com>
2023-09-26 19:48:55 +0200

    Huge update on task dialog

--- components/CreateTaskDialog.tsx
+++ components/CreateTaskDialog.tsx
@@ -5,6 +5,16 @@ import React from 'react';
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from './ui/dialog';
 import { cn } from '@/lib/utils';
 import { CollectionColor, CollectionColors } from '@/lib/constants';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from './ui/form';
+import { createCollectionSchemaType } from '@/schema/createCollection';
+import { createTaskSchema } from '@/schema/createTask';
+import { Textarea } from './ui/textarea';
+import { Popover, PopoverContent, PopoverTrigger } from './ui/popover';
+import { Calendar } from './ui/calendar';
+import { Button } from './ui/button';
+import { CalendarIcon } from '@radix-ui/react-icons';
 
 interface Props {
   open: boolean;
@@ -13,8 +23,19 @@ interface Props {
 }
 
 function CreateTaskDialog({ open, setOpen, collection }: Props) {
+  const form = useForm<createCollectionSchemaType>({
+    resolver: zodResolver(createTaskSchema),
+    defaultValues: {
+      collectionId: collection.id,
+    },
+  })
+
   const openChangeWrapper = (value: boolean) => {
     setOpen(value);
+  }
+
+  const onSubmit = async (data: createTaskSchemaType) => {
+    console.log("SUBMITTED", data)
   }
 
   return (
@@ -37,6 +58,74 @@ function CreateTaskDialog({ open, setOpen, collection }: Props) {
             Add a task to your collection. You can add as many tasks as you want to a collection.
           </DialogDescription>
         </DialogHeader>
+
+        <div>
+          <Form {...form}>
+            <form
+              className='space-y-4 flex flex-col'
+              onSubmit={form.handleSubmit(onSubmit)}
+            >
+              <FormField
+                control={form.control}
+                name="content"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>
+                      Content
+                    </FormLabel>
+
+                    <FormControl>
+                      <Textarea
+                        rows={5}
+                        placeholder='Task content here'
+                        {...field}
+                      />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="expiresAt"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>
+                      Expires at
+                    </FormLabel>
+
+                    <FormDescription>
+                      When should this task expire?
+                    </FormDescription>
+
+                    <FormControl>
+                      <Popover>
+                        <PopoverTrigger asChild>
+                          <Button>
+                            <CalendarIcon className='mr-2 h-4 w-4' />
+                          </Button>
+                        </PopoverTrigger>
+
+                        <PopoverContent>
+                          <Calendar
+                            mode="single"
+                            selected={field.value}
+                            onSelect={field.onChange}
+                            initialFocus
+                          />
+                        </PopoverContent>
+                      </Popover>
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </form>
+          </Form>
+        </div>
       </DialogContent>
     </Dialog>
   )
