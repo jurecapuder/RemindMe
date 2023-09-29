4a662716098bd67fefba4bae10dc5199f2070343
jurecapuder <capuder.j@gmail.com>
2023-09-29 19:31:23 +0200

    Adding task card component

--- components/TaskCard.tsx
+++ components/TaskCard.tsx
@@ -1,10 +1,27 @@
 "use client";
 import { Task } from '@prisma/client';
 import React from 'react'
+import { Checkbox } from './ui/checkbox';
+import { format } from 'date-fns';
+import { cn } from '@/lib/utils';
 
 function TaskCard({task}:{task:Task}) {
   return (
-    <div>Task Card</div>
+    <div>
+      <Checkbox />
+
+      <label>
+        {task.content}
+        {task.expiresAt && (
+          <p className={cn(
+            "text-xs text-neutral-500 dark:text-neutral-400",
+            getExpirationColor(task.expiresAt)
+          )}>
+            {format(task.expiresAt, "dd/MM/yyyy")}
+          </p>
+        )}
+      </label>
+    </div>
   )
 }
 
