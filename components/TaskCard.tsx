e543a162e1aef071c71675884396b4fe5e422a72
jurecapuder <capuder.j@gmail.com>
2023-09-29 19:37:39 +0200

    Adding getExpirationColor function

--- components/TaskCard.tsx
+++ components/TaskCard.tsx
@@ -5,6 +5,18 @@ import { Checkbox } from './ui/checkbox';
 import { format } from 'date-fns';
 import { cn } from '@/lib/utils';
 
+function getExpirationColor(expiresAt: Date) {
+  const days = Math.floor(expiresAt.getTime() - Date.now()) / 1000 / 60 / 60;
+
+  if (days < 0) return "text-gray-500 dark:text-gray-400"
+
+  if (days <= 3 * 24) return "text-red-500 dark:text-red-400";
+
+  if (days <= 7 * 24) return "text-orange-500 dark:text-orange-400";
+
+  return "text-green-500 dark:text-green-400";
+}
+
 function TaskCard({task}:{task:Task}) {
   return (
     <div>
