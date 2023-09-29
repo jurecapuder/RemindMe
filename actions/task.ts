5d1e857802311e5890a1782dac2fa59fe320bcc0
jurecapuder <capuder.j@gmail.com>
2023-09-29 20:20:18 +0200

    Updating task server action

--- actions/task.ts
+++ actions/task.ts
@@ -1,45 +1,46 @@
 "use server";
 
 import prisma from "@/lib/prisma";
-import { createTaskSchema } from "@/schema/createTask";
+import { createTaskSchemaType } from "@/schema/createTask";
 import { currentUser } from "@clerk/nextjs";
 
-export async function createTask(data: createTaskSchema) {
-  const user = await currentUser();
+export async function createTask(data: createTaskSchemaType){
+    const user = await currentUser();
 
-  if (!user) {
-    throw new Error("User not found");
-  }
-
-  const { content, expiresAt, collectionId } = data;
-
-  return await prisma.task.create({
-    data: {
-      userId: user.id,
-      content,
-      expiresAt,
-      Collection: {
-        id: collectionId
-      }
-    },
-  });
-
+    if (!user) {
+        throw new Error("user not found")
+    }
+    
+    const {content, expiresAt, collectionId} = data;
+    
+    return await prisma.task.create({
+        data: {
+            userId: user.id,
+          content,
+          expiresAt,
+          Collection: {
+            connect: {
+                id: collectionId
+            }
+          }
+        }
+    })
 }
 
 export async function setTaskToDone(id: number) {
-  const user = await currentUser();
+    const user = await currentUser();
 
-  if (!user) {
-    throw new Error("User not found");
-  }
+    if (!user) {
+        throw new Error("user not found")
+    } 
 
-  return await prisma.task.update({
-    where: {
-      id: id,
-      userId: user.id,
-    },
-    data: {
-      done: true
-    }
-  });
+    return await prisma.task.update({
+        where: {
+            id: id,
+            userId: user.id
+        },
+        data: {
+            done: true
+        }
+    })
 }
