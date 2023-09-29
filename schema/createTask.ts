c1052fd2f3bfe135e73f5703a3c552bb3bd685cc
jurecapuder <capuder.j@gmail.com>
2023-09-29 20:14:38 +0200

    Updating task

--- schema/createTask.ts
+++ schema/createTask.ts
@@ -1,11 +1,11 @@
-import {z} from 'zod';
+import {z} from 'zod'
 
 export const createTaskSchema = z.object({
   collectionId: z.number().nonnegative(),
   content: z.string().min(8, {
-    message: "Task content must be at least 8 characters long"
+    message: "Task content must be at least 8 characters"
   }),
-  expiresAt: z.string().optional(),
-});
+  expiresAt: z.string().optional()
+})
 
-export type createTaskSchema = z.infer<typeof createTaskSchema>;
+export type createTaskSchemaType = z.infer<typeof createTaskSchema>
