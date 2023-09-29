e9c971a38b9fd6f5339b1b0dffceb7a1b5974a58
jurecapuder <capuder.j@gmail.com>
2023-09-29 19:40:42 +0200

    Adding classname to components

--- components/TaskCard.tsx
+++ components/TaskCard.tsx
@@ -19,10 +19,17 @@ function getExpirationColor(expiresAt: Date) {
 
 function TaskCard({task}:{task:Task}) {
   return (
-    <div>
-      <Checkbox />
+    <div className='flex gap-2 items-start'>
+      <Checkbox
+        id={task.id.toString()}
+        className='w-5 h-5'
+        checked={task.done}
+      />
 
-      <label>
+      <label htmlFor={task.id.toString()} className={cn(
+        "text-sm font-medium leading-none near-disabled:cursor-not-allowed peer-disabled:opacity-70 decoration-1 dark:decoration-white",
+        task.done && "line-through"
+      )}>
         {task.content}
         {task.expiresAt && (
           <p className={cn(
